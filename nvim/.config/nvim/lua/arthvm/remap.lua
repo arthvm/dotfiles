48dc96535302ac80a29c537cdf992bc78613fbe4
Arthur Mariano <90999445+arthvm@users.noreply.github.com>
2024-10-28 21:50:53 -0300

    feat: add config to work with tmux

--- nvim/.config/nvim/lua/arthvm/remap.lua
+++ nvim/.config/nvim/lua/arthvm/remap.lua
@@ -1,3 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
+vim.keymap.set('n', '<A-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<A-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<A-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<A-l>', ':wincmd l<CR>')
