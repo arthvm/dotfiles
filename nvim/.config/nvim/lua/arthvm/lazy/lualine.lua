de7b2eb83daccd3960ec951e8d5e280ee4fe8a90
Arthur Mariano <90999445+arthvm@users.noreply.github.com>
2025-02-12 16:30:53 -0300

    feat: improve nvim usability

--- nvim/.config/nvim/lua/arthvm/lazy/lualine.lua
+++ nvim/.config/nvim/lua/arthvm/lazy/lualine.lua
@@ -1,11 +1,15 @@
 return {
+  {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('lualine').setup({
-        options = {
-          theme = 'nightfly'
-        }
-      })
-    end
+    event = "VeryLazy",
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = '|',
+        section_separators = { left = '', right = '' },
+      },
+    },
+  },
 }
